f904fc68127bc75b8b3869070b03151013eacf38
Richard Sinclair <richardksinclair20@gmail.com>
2024-02-29 15:11:23 -0600

    Header CSS done for now; added react-social-media-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="Footer">
-    <p>&copy; Richard Sinclair *&nbsp; </p>
-      <a href="https://www.github.com/DudeLovesRPGs">My Github</a>
+    <p>&copy; Richard Sinclair</p>
     </footer>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {ReactSocialMediaIcons} from 'react-social-media-icons';
+
 
 const Header = () => {
     return(<header className='Header'>
     <p>Uhmeegle</p>
-    <button id='FBShareButton'>Share</button>
-    <button id='TwitterShareButton'>Tweet</button>
+    {/* <button id='FBShareButton'>Share</button>
+    <button id='TwitterShareButton'>Tweet</button> */}
+    <ReactSocialMediaIcons icon = "github" url = "https://www.github.com/DudeLovesRPGs"  borderColor = "rgba(255, 255, 255, 0)" size = "22"/>
+    <ReactSocialMediaIcons icon = "linkedin" url = "https://www.linkedin.com/in/richard-sinclair-dlrpgs" borderColor = "rgba(255,255,255,0)" size = "22"/>
     
 </header>)
 }
