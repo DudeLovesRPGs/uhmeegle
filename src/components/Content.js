1717b7afb6c56d3f86c22ac57c8f5c1887fefc02
Richard Sinclair <richardksinclair20@gmail.com>
2024-03-01 16:04:17 -0600

    Made logo click redirect to home; made Text button go to chat page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-/*TODO [ChatPage]: Wire up 'Text' button to go to ChatPage*/
+/*DONE [ChatPage]: Wire up 'Text' button to go to ChatPage*/
 /*TODO [HomePage]: Make 'Interests' field work like on original website*/
 
 
 
 const Content = () => {
+
+    const navigate = useNavigate()
 
     return(<section className='Content'>
     <p>Uhmeegle <i>(uh-mee-gul)</i> is a faithful recreation of the similarly named app that was online from 2009-2023. For right now, it exists as a recreation, but new features may be added in the future. All features from the original app are intact. Chats are anonymous, but please do reveal personal information such as your name, address, or other identifying information.</p>
@@ -21,7 +24,9 @@ const Content = () => {
 
 
     <div id='buttonRow'>
-    <button>Text</button>
+    <button onClick={() => {
+        navigate('/chat')
+    }}>Text</button>
     <span>or</span>
     <button>Video</button>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
 import {ReactSocialMediaIcons} from 'react-social-media-icons';
 
 
+
 const Header = () => {
+    const navigate = useNavigate()
+
     return(<header className='Header'>
-    <p>Uhmeegle</p>
+    <p onClick={() =>{navigate('/')}}>Uhmeegle</p>
     {/* <button id='FBShareButton'>Share</button>
     <button id='TwitterShareButton'>Tweet</button> */}
     <ReactSocialMediaIcons icon = "github" url = "https://www.github.com/DudeLovesRPGs"  borderColor = "rgba(255, 255, 255, 0)" size = "22"/>
